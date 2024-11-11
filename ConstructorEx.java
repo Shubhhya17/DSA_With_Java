b0a381527ac8ded9451194cf496a4bce0d0d2b4d
Shubham Pawar <shubhpawar1703@gmail.com>
2024-11-11 20:21:08 +0530

    DSA

---
+++ ConstructorEx.java
@@ -0,0 +1,6 @@
+class ConstructorEx{
+    int i;
+    public static void main(String[] args){
+        ConstructorEx ex = new ConstructorEx();
+    }
+}
---
+++ LenghtAndLenght.java
@@ -0,0 +1,10 @@
+// lenght()
+public class LenghtAndLenght {
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int[] a = { 20, 30, 40 };
+        System.err.println(a.length);
+      System.err.println( str.length());
+    }
+}
