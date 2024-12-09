6ee85bc9e7a18eab6ad626994d302398eac00414
Shubham Pawar <shubhpawar1703@gmail.com>
2024-12-09 21:33:18 +0530

    Pattern01

---
+++ Pattern01.java
@@ -0,0 +1,17 @@
+// *****
+// *****
+// *****
+// *****
+// *****
+
+public class Pattern01 {
+    public static void main(String args[]){
+        for(int i=0; i<5;i++){
+            System.out.print("*");
+            for(int j =1; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
